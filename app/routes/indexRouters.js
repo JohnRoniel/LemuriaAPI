fb0692734f156dded14ecb3f94069e23b1dd2dd2
JRoniel <jroniel055@gmail.com>
2025-01-14 23:18:26 -0300

    :sparkles: New error route's added

--- app/routes/indexRouters.js
+++ app/routes/indexRouters.js
@@ -1,4 +1,18 @@
 module.exports = (app) => {
+	// 404 Error Route
+	app.use((req, res, next) => {
+		res.status(404).json("Not Found Route - 404");
+	});
+
+	// 404 Route not estabelished
+	app.get("/", (req, res) => {
+		res.status(404).json("Not Found Route - 404");
+	});
+
+	/**
+	 * API Route
+	 * @route GET /api
+	 */
 	app.get("/api", (req, res) => {
 		res.send("Welcome to the Lemuria API");
 	});
